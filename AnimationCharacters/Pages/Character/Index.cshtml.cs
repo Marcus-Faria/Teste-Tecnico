3e194c6704f9eebe8bd03245faa1ccb177e5efa0
Marcus-Faria <marcusviniciusfaria150@gmail.com>
2021-11-26 23:42:53 -0300

    Added search tab

--- AnimationCharacters/Pages/Character/Index.cshtml.cs
+++ AnimationCharacters/Pages/Character/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AnimationCharacters.Models;
 using AnimationCharacters.Data;
@@ -19,11 +20,30 @@ namespace AnimationCharacters.Pages_Character
             _context = context;
         }
 
-        public IList<Character> Character { get;set; }
+        public IList<Character> Character { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Categories { get; set; }
+        public string CtgCharacter { get; set; }
 
         public async Task OnGetAsync()
         {
-            Character = await _context.Character.ToListAsync();
+            // Use LINQ to get list of Categories.
+            IQueryable<string> categoryQuery = from m in _context.Character orderby m.CategoryCharacter select m.CategoryCharacter;
+
+            var characters = from m in _context.Character select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+               characters = characters.Where(s => s.TitleMovie.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(CtgCharacter))
+            {
+                characters = characters.Where(x => x.CategoryCharacter == CtgCharacter);
+            }
+
+            Categories = new SelectList(await categoryQuery.Distinct().ToListAsync());
+            Character = await characters.ToListAsync();
         }
     }
 }
