0b553a4b82b25bb3719f58eb16b8674af67b70ab
Marcus-Faria <marcusviniciusfaria150@gmail.com>
2021-11-30 15:49:34 -0300

    commented Regular Expression

--- AnimationCharacters/Models/Character.cs
+++ AnimationCharacters/Models/Character.cs
@@ -8,30 +8,30 @@ namespace AnimationCharacters.Models
     {
         public int ID { get; set; }
 
-        [RegularExpression(@"^[a-zA-Z0-9\s-:?!ºª+&%#]*$")]
+        //[RegularExpression(@"^[a-zA-Z0-9\s-:?!ºª+&%#]*$")]
         [Display(Name = "Title")]
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string TitleMovie { get; set; }
 
-        [Range(typeof(DateTime), "1/1/1900", "1/1/2200")]
+        //[Range(typeof(DateTime), "1/1/1900", "1/1/2200")]
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9\s-]*$")]
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z0-9\s-]*$")]
         [Display(Name = "Character")]
         [StringLength(30)]
         [Required]
         public string NameCharacter { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
         [Display(Name = "Category")]
         [StringLength(20)]
         [Required]
         public string CategoryCharacter { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z/s-]*$")]
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z/s-]*$")]
         [StringLength(30)]
         [Required]
         public string Scenery { get; set; }
